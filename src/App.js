9a66321c58d8203ac1656af6554537b60532fdeb
NishatFariza <nishatfarzia78@gmail.com>
2022-04-17 03:29:42 +0600

    react bootstrap add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route to='/'></Route>
+      </Routes>
     </div>
   );
 }
