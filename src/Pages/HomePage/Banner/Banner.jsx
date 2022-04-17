def7fad9adb39a709970f81cfe99dad8fa57bc6d
NishatFariza <nishatfarzia78@gmail.com>
2022-04-17 19:26:48 +0600

    banner responsive add

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -4,18 +4,25 @@ import bannerImg from "../../../images/hero-lawyer-image.png";
 
 const Banner = () => {
   return (
-    <div className="banner d-flex justify-content-center align-items-center">
-      <div className="banner-content-wrap text-center text-white">
-        <p className="banner-caption primary-font text-uppercase">
+    <div className="banner d-flex flex-column flex-md-row justify-content-center align-items-center">
+      <div className="banner-content-wrap text-center text-white mb-5">
+        <p className="banner-caption text-uppercase">
           skipping bail - since 2020
         </p>
-        <h1 className="primary-font">
+        <h1 className="primary-font mb-3">
           In The Halls Of Justice
-          <span className="primary-color">
-               The <br /> Only Justice
-          </span>
-          Is In The <br /> Halls.
+          <span className="primary-color">The Only Justice</span>
+          Is In The Halls.
         </h1>
+        <div className="divider"></div>
+        <div className="btn-wrap mt-4">
+          <button className="py-3 px-4 border-0 rounded banner-btn text-white fw-bold me-3">
+            get a free quote
+          </button>
+          <button className="py-3 px-4 bg-0 rounded banner-btn-2 text-white fw-bold">
+            legal services
+          </button>
+        </div>
       </div>
       <div className="banner-img-wrap d-flex justify-content-center align-items-center">
         <img className="w-100" src={bannerImg} alt="" />
--- src/Pages/HomePage/Services/Services.jsx
+++ src/Pages/HomePage/Services/Services.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Services = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>This is Service</h1>
+    </div>
+  );
 };
 
 export default Services;
