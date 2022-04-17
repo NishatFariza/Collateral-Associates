4df9a35aea45ec25835081d1e4ac27912531e065
NishatFariza <nishatfarzia78@gmail.com>
2022-04-18 01:26:54 +0600

    service card style

--- src/Pages/HomePage/Service/Service.jsx
+++ src/Pages/HomePage/Service/Service.jsx
@@ -7,11 +7,16 @@ const Service = ({ service }) => {
 
   return (
     <div className="service-card mx-auto d-flex justify-content-center align-items-center flex-column">
-      <div className="img-wrap rounded-circle bg-light">
-        <img src={img} alt="" />
+      <div className="img-wrap rounded-circle overflow-hidden d-flex justify-content-center align-items-center">
+        <img className="w-50 h-50 rounded-circle " src={img} alt="" />
       </div>
       <h5 className="text-white">{name}</h5>
-      <p>{discription}</p>
+      <p title={discription} className="text-light">
+        {discription.length >= 150
+          ? `${discription.slice(0, 150)}...`
+          : discription}
+      </p>
+      <span className="text-light">${price}</span>
       <button>Booking Now</button>
     </div>
   );
