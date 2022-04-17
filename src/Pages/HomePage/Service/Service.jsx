0cec33bb622fe735aceb30c66be1b5a97b9713f0
NishatFariza <nishatfarzia78@gmail.com>
2022-04-18 00:44:42 +0600

    service card add

--- src/Pages/HomePage/Service/Service.jsx
+++ src/Pages/HomePage/Service/Service.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import "./Service.css";
 
 const Service = ({ service }) => {
   console.log(service);
   const { img, name, price, discription } = service;
 
   return (
-    <div>
-      <h2>ffffffffffffff</h2>
-      <img src={img} alt="" />
-      <h4>{name}</h4>
+    <div className="service-card mx-auto d-flex justify-content-center align-items-center flex-column">
+      <div className="img-wrap rounded-circle bg-light">
+        <img src={img} alt="" />
+      </div>
+      <h5 className="text-white">{name}</h5>
+      <p>{discription}</p>
+      <button>Booking Now</button>
     </div>
   );
 };
--- src/Pages/HomePage/Services/Services.jsx
+++ src/Pages/HomePage/Services/Services.jsx
@@ -13,11 +13,11 @@ const Services = () => {
   // console.log(services);
   return (
     <div className="p-5">
-      <div className="services-wrap py-5 rounded-3">
+      <div className="services-wrap pt-5 rounded-3">
         <div className="services-title-wrap mx-4">
           <h2 className="primary-font text-white p-4">Services</h2>
         </div>
-        <div className="services-grid-container mx-auto my-5">
+        <div className="services-grid-container mx-4 pb-5">
           {services.map((service) => (
             <Service key={service.id} service={service}></Service>
           ))}
