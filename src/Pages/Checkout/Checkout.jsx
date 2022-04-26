243cf1efbeff1b4381bece6a953383d0a738cee9
NishatFariza <nishatfarzia78@gmail.com>
2022-04-27 05:10:42 +0600

    checkout form animation add

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Col, Form, InputGroup, Row } from "react-bootstrap";
+import { Fade } from "react-reveal";
 import "./Checkout.css";
 
 const Checkout = () => {
@@ -15,74 +16,78 @@ const Checkout = () => {
     setValidated(true);
   };
   return (
-    <div className="w-50 mx-auto my-5 shadow-lg p-5">
-      <h4 className="primary-text primary-color text-center mb-4">CheckOut</h4>
-      <div>
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="4" controlId="validationCustom01">
-              <Form.Label>First name</Form.Label>
-              <Form.Control required type="text" placeholder="First name" />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationCustom02">
-              <Form.Label>Last name</Form.Label>
-              <Form.Control required type="text" placeholder="Last name" />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-              <Form.Label>Username</Form.Label>
-              <InputGroup hasValidation>
-                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                <Form.Control
-                  type="text"
-                  placeholder="Username"
-                  aria-describedby="inputGroupPrepend"
-                  required
-                />
+    <Fade bottom>
+      <div className="w-lg-50 w-50 mx-auto my-5 shadow-lg p-5">
+        <h4 className="primary-text primary-color text-center mb-4">
+          CheckOut
+        </h4>
+        <div>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row className="mb-3">
+              <Form.Group as={Col} md="4" controlId="validationCustom01">
+                <Form.Label>First name</Form.Label>
+                <Form.Control required type="text" placeholder="First name" />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="4" controlId="validationCustom02">
+                <Form.Label>Last name</Form.Label>
+                <Form.Control required type="text" placeholder="Last name" />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+                <Form.Label>Username</Form.Label>
+                <InputGroup hasValidation>
+                  <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                  <Form.Control
+                    type="text"
+                    placeholder="Username"
+                    aria-describedby="inputGroupPrepend"
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please choose a username.
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} md="6" controlId="validationCustom03">
+                <Form.Label>City</Form.Label>
+                <Form.Control type="text" placeholder="City" required />
                 <Form.Control.Feedback type="invalid">
-                  Please choose a username.
+                  Please provide a valid city.
                 </Form.Control.Feedback>
-              </InputGroup>
+              </Form.Group>
+              <Form.Group as={Col} md="3" controlId="validationCustom04">
+                <Form.Label>State</Form.Label>
+                <Form.Control type="text" placeholder="State" required />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid state.
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="3" controlId="validationCustom05">
+                <Form.Label>Zip</Form.Label>
+                <Form.Control type="text" placeholder="Zip" required />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid zip.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Form.Group className="mb-3">
+              <Form.Check
+                required
+                label="Agree to terms and conditions"
+                feedback="You must agree before submitting."
+                feedbackType="invalid"
+              />
             </Form.Group>
-          </Row>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="6" controlId="validationCustom03">
-              <Form.Label>City</Form.Label>
-              <Form.Control type="text" placeholder="City" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid city.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationCustom04">
-              <Form.Label>State</Form.Label>
-              <Form.Control type="text" placeholder="State" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid state.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationCustom05">
-              <Form.Label>Zip</Form.Label>
-              <Form.Control type="text" placeholder="Zip" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid zip.
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-          <Form.Group className="mb-3">
-            <Form.Check
-              required
-              label="Agree to terms and conditions"
-              feedback="You must agree before submitting."
-              feedbackType="invalid"
-            />
-          </Form.Group>
-          <button className="banner-btn rounded" type="submit">
-            Check Out
-          </button>
-        </Form>
+            <button className="checkout-btn rounded" type="submit">
+              Check Out
+            </button>
+          </Form>
+        </div>
       </div>
-    </div>
+    </Fade>
   );
 };
 
