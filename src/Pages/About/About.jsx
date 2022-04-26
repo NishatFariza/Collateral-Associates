1318c643ed0ae15189db3e9c28136be2486c2cbc
NishatFariza <nishatfarzia78@gmail.com>
2022-04-27 01:54:45 +0600

    about section edit

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -5,12 +5,13 @@ const About = () => {
     <div className="py-lg-5 py-3 w-75 mx-auto">
       <h2 className="text-center primary-color">About Me</h2>
       <p className="text-muted py-3 text-center">
-        I'm Frontend, MERN Stack Developer! I am an enthusiastic and dedicated
-        learner. with a deep interest in JavaScript. To work in the Software
-        industry with modern web technologies web 3.0 and To pursue a
-        challenging job in a dynamic and esteemed organization, where I will be
-        able to perform in any kind of situation with full effort to utilize my
-        knowledge, skill, and experience for professional career development..
+        Hello There, Welcome! Nishat Fariza,I'm Frontend, MERN Stack Developer!
+        I am an enthusiastic and dedicated learner. with a deep interest in
+        JavaScript. To work in the Software industry with modern web
+        technologies web 3.0 and To pursue a challenging job in a dynamic and
+        esteemed organization, where I will be able to perform in any kind of
+        situation with full effort to utilize my knowledge, skill, and
+        experience for professional career development..
       </p>
     </div>
   );
