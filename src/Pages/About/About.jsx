a9999e9bb7f75edff424e46d7328d171b095cd6b
NishatFariza <nishatfarzia78@gmail.com>
2022-04-27 02:28:54 +0600

    checkout form add

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,15 +3,16 @@ import React from "react";
 const About = () => {
   return (
     <div className="py-lg-5 py-3 w-75 mx-auto">
-      <h2 className="text-center primary-color">About Me</h2>
+      <h2 className="text-center primary-color primary-font">About Me</h2>
       <p className="text-muted py-3 text-center">
-        Hello There, Welcome! Nishat Fariza,I'm Frontend, MERN Stack Developer!
-        I am an enthusiastic and dedicated learner. with a deep interest in
-        JavaScript. To work in the Software industry with modern web
-        technologies web 3.0 and To pursue a challenging job in a dynamic and
-        esteemed organization, where I will be able to perform in any kind of
-        situation with full effort to utilize my knowledge, skill, and
-        experience for professional career development..
+        Hello There, Welcome! This is{" "}
+        <span className="primary-color primary-font"> Nishat Fariza</span>. I'm
+        Frontend, MERN Stack Developer! I am an enthusiastic and dedicated
+        learner. with a deep interest in JavaScript. To work in the Software
+        industry with modern web technologies web 3.0 and To pursue a
+        challenging job in a dynamic and esteemed organization, where I will be
+        able to perform in any kind of situation with full effort to utilize my
+        knowledge, skill, and experience for professional career development..
       </p>
     </div>
   );
