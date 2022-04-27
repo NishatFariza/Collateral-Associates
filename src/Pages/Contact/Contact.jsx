e8b1985be653b474a8fc5d0ef0a1af4c08acf090
NishatFariza <nishatfarzia78@gmail.com>
2022-04-27 06:31:53 +0600

    contact component add

---
+++ src/Pages/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>This is contact</h2>
+    </div>
+  );
+};
+
+export default Contact;
